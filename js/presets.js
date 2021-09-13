1d81dda7bceda6c9343dd10e2a5973cc5b3cd12e
Melis <59087686+melis@users.noreply.github.com>
2021-09-14 00:58:14 +0600

    tablet2

--- js/presets.js
+++ js/presets.js
@@ -14,7 +14,7 @@
 $(document).ready(function () {
   $(".burger, .shtor,  .close_menu").click(function (event) {
     console.log(event);
-    $(".burger").toggleClass("active");
+    $(".burger").toggleClass("burger_active");
     $(".nav").toggleClass("active_nav");
     $(".shtor").toggleClass("active_shtor");
     $("body").toggleClass("body_scrl");
