5611c5a340e6a715a5ca494b96da153fc6ed5ff9
Melis <59087686+melis@users.noreply.github.com>
2021-09-17 22:12:59 +0600

    copy

--- js/presets.js
+++ js/presets.js
@@ -1,57 +1,73 @@
 (() => {
-    Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-        get: function() {
-            return !!(
-                this.currentTime > 0 &&
-                !this.paused &&
-                !this.ended &&
-                this.readyState > 2
-            );
-        },
-    });
+  Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+    get: function () {
+      return !!(
+        this.currentTime > 0 &&
+        !this.paused &&
+        !this.ended &&
+        this.readyState > 2
+      );
+    },
+  });
 })();
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $(".burger, .shtor,  .close_menu").click(function (event) {
+    $(".burger").toggleClass("burger_active");
+    $(".nav").toggleClass("active_nav");
+    $(".shtor").toggleClass("active_shtor");
+    $("body").toggleClass("body_scrl");
+    $(".close_menu").toggleClass("close_menu_active ");
+    $(".language").toggleClass("language_active");
+  });
 
-    $(".burger, .shtor,  .close_menu").click(function(event) {
-        $(".burger").toggleClass("burger_active");
-        $(".nav").toggleClass("active_nav");
-        $(".shtor").toggleClass("active_shtor");
-        $("body").toggleClass("body_scrl");
-        $(".close_menu").toggleClass("close_menu_active ");
-        $('.language').toggleClass('language_active')
-    });
+  $(".video_card__img, .video_modal__close").click(function () {
+    $(".video_modal").toggleClass("video_modal__active");
+    $("body").toggleClass("body_scrl");
 
-    $('.video_card__img, .video_modal__close').click(function() {
-        $('.video_modal').toggleClass('video_modal__active')
-        $("body").toggleClass("body_scrl");
+    // let e = $(".video__play")
+    // console.log(e)
+    // if (e.currentTarget.playing) {
+    //     e.currentTarget.pause();
+    // console.log($(e.currentTarget).find(".video__play"));
+    // $(e.currentTarget)
+    //     .closest(".video")
+    //     .find(".video__play")
+    //     .removeClass("video__play_hidden");
+    // }
+  });
 
-        // let e = $(".video__play")
-        // console.log(e)
-        // if (e.currentTarget.playing) {
-        //     e.currentTarget.pause();
-        // console.log($(e.currentTarget).find(".video__play"));
-        // $(e.currentTarget)
-        //     .closest(".video")
-        //     .find(".video__play")
-        //     .removeClass("video__play_hidden");
-        // }
+  $(".copy_link").on("click", (e) => {
+    if (e.target.dataset.link) {
+      navigator.clipboard.writeText(e.target.dataset.link).then(() => {
+        e.target.className = "primary";
+        e.target.innerText = "Скопировано";
+      });
+    } else {
+      e.target.className = "danger";
+      e.target.innerText = "Ссылка пуста";
+    }
+    setTimeout(() => {
+      e.target.className = "copy_link";
+      e.target.innerText = "Скопировать";
+    }, 2000);
+  });
 
-    })
-
-    var button = $('#button-up');
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 300) {
-            button.fadeIn();
-        } else {
-            button.fadeOut();
-        }
-    });
-    button.on('click', function() {
-        $('body, html').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
-
+  var button = $("#button-up");
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+  button.on("click", function () {
+    $("body, html").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+    return false;
+  });
 });
