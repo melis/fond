14c275824ac72b35e955826044937952b4b46b2f
Melis <59087686+melis@users.noreply.github.com>
2021-09-17 11:24:00 +0600

    up

--- js/presets.js
+++ js/presets.js
@@ -1,30 +1,47 @@
 (() => {
-  Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-    get: function () {
-      return !!(
-        this.currentTime > 0 &&
-        !this.paused &&
-        !this.ended &&
-        this.readyState > 2
-      );
-    },
-  });
+    Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+        get: function() {
+            return !!(
+                this.currentTime > 0 &&
+                !this.paused &&
+                !this.ended &&
+                this.readyState > 2
+            );
+        },
+    });
 })();
 
-$(document).ready(function () {
-  $(".burger, .shtor,  .close_menu").click(function (event) {
-    console.log(event);
-    $(".burger").toggleClass("burger_active");
-    $(".nav").toggleClass("active_nav");
-    $(".shtor").toggleClass("active_shtor");
-    $("body").toggleClass("body_scrl");
-    $(".close_menu").toggleClass("close_menu_active ");
-    $('.language').toggleClass('language_active')
-  });
+$(document).ready(function() {
 
-  $('.video_card__img, .video_modal__close').click(function(){
-    $('.video_modal').toggleClass('video_modal__active')
-      $("body").toggleClass("body_scrl");
-      
-  })
+    $(".burger, .shtor,  .close_menu").click(function(event) {
+        console.log(event);
+        $(".burger").toggleClass("burger_active");
+        $(".nav").toggleClass("active_nav");
+        $(".shtor").toggleClass("active_shtor");
+        $("body").toggleClass("body_scrl");
+        $(".close_menu").toggleClass("close_menu_active ");
+        $('.language').toggleClass('language_active')
+    });
+
+    $('.video_card__img, .video_modal__close').click(function() {
+        $('.video_modal').toggleClass('video_modal__active')
+        $("body").toggleClass("body_scrl");
+
+    })
+
+    var button = $('#button-up');
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+    button.on('click', function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
 });
