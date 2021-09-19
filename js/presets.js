22f4a454fca9daa6c47254f3026ed4b9164e1f9f
Melis <59087686+melis@users.noreply.github.com>
2021-09-19 13:45:14 +0600

    spider

--- js/presets.js
+++ js/presets.js
@@ -1,73 +1,80 @@
 (() => {
-  Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-    get: function () {
-      return !!(
-        this.currentTime > 0 &&
-        !this.paused &&
-        !this.ended &&
-        this.readyState > 2
-      );
-    },
-  });
+    Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+        get: function() {
+            return !!(
+                this.currentTime > 0 &&
+                !this.paused &&
+                !this.ended &&
+                this.readyState > 2
+            );
+        },
+    });
 })();
 
-$(document).ready(function () {
-  $(".burger, .shtor,  .close_menu").click(function (event) {
-    $(".burger").toggleClass("burger_active");
-    $(".nav").toggleClass("active_nav");
-    $(".shtor").toggleClass("active_shtor");
-    $("body").toggleClass("body_scrl");
-    $(".close_menu").toggleClass("close_menu_active ");
-    $(".language").toggleClass("language_active");
-  });
+$(document).ready(function() {
+    const video = $(" video")[0]
+    $(".burger, .shtor,  .close_menu").click(function(event) {
+        $(".burger").toggleClass("burger_active");
+        $(".nav").toggleClass("active_nav");
+        $(".shtor").toggleClass("active_shtor");
+        $("body").toggleClass("body_scrl");
+        $(".close_menu").toggleClass("close_menu_active ");
+        $(".language").toggleClass("language_active");
+    });
 
-  $(".video_card__img, .video_modal__close").click(function () {
-    $(".video_modal").toggleClass("video_modal__active");
-    $("body").toggleClass("body_scrl");
+    $(".video_modal__close").on('click', function() {
 
-    // let e = $(".video__play")
-    // console.log(e)
-    // if (e.currentTarget.playing) {
-    //     e.currentTarget.pause();
-    // console.log($(e.currentTarget).find(".video__play"));
-    // $(e.currentTarget)
-    //     .closest(".video")
-    //     .find(".video__play")
-    //     .removeClass("video__play_hidden");
-    // }
-  });
+        $(".video_modal").removeClass("video_modal__active");
+        $("body").removeClass("body_scrl");
+        video.pause()
 
-  $(".copy_link").on("click", (e) => {
-    if (e.target.dataset.link) {
-      navigator.clipboard.writeText(e.target.dataset.link).then(() => {
-        e.target.className = "primary";
-        e.target.innerText = "Скопировано";
-      });
-    } else {
-      e.target.className = "danger";
-      e.target.innerText = "Ссылка пуста";
-    }
-    setTimeout(() => {
-      e.target.className = "copy_link";
-      e.target.innerHTML = 'Копировать <img src="/images/copy.svg" alt="">';
-    }, 2000);
-  });
+    });
 
-  var button = $("#button-up");
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      button.fadeIn();
-    } else {
-      button.fadeOut();
-    }
-  });
-  button.on("click", function () {
-    $("body, html").animate(
-      {
-        scrollTop: 0,
-      },
-      800
-    );
-    return false;
-  });
+    $('.play').on('click', e => {
+        if (e.currentTarget.dataset.movie__link) {
+            video.src = e.currentTarget.dataset.movie__link;
+        } else {
+            video.src = "./images/movie.mp4"
+        }
+
+        $(".video_modal").addClass("video_modal__active");
+        $("body").addClass("body_scrl");
+        $(".video__play").addClass('video__play_hidden')
+        video.play()
+    })
+
+
+
+    $(".copy_link").on("click", (e) => {
+        if (e.target.dataset.link) {
+            navigator.clipboard.writeText(e.target.dataset.link).then(() => {
+                e.target.className = "primary";
+                e.target.innerText = "Скопировано";
+            });
+        } else {
+            e.target.className = "danger";
+            e.target.innerText = "Ссылка пуста";
+        }
+        setTimeout(() => {
+            e.target.className = "copy_link";
+            e.target.innerHTML = 'Копировать <img src="/images/copy.svg" alt="">';
+        }, 2000);
+    });
+
+    var button = $("#button-up");
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+    button.on("click", function() {
+        $("body, html").animate({
+                scrollTop: 0,
+            },
+            800
+        );
+        return false;
+    });
 });
