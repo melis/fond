39a3d8ea82fa65f6e6d9c4d608b8e13281c446e9
Melis <59087686+melis@users.noreply.github.com>
2021-09-19 16:05:40 +0600

    img

--- js/presets.js
+++ js/presets.js
@@ -77,4 +77,15 @@ $(document).ready(function() {
         );
         return false;
     });
+
+
+
+    $(".images img").click(function() {
+        $("#full-image").attr("src", $(this).attr("src"));
+        $('#image-viewer').show();
+    });
+
+    $("#image-viewer .close").click(function() {
+        $('#image-viewer').hide();
+    });
 });
