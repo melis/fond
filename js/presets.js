c4a6c7d0778e203851fbf6ee2b8f435594932b0b
Melis <59087686+melis@users.noreply.github.com>
2021-09-20 13:05:15 +0600

    ds

--- js/presets.js
+++ js/presets.js
@@ -1,86 +1,98 @@
 (() => {
-  Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-    get: function () {
-      return !!(
-        this.currentTime > 0 &&
-        !this.paused &&
-        !this.ended &&
-        this.readyState > 2
-      );
-    },
-  });
+    Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+        get: function() {
+            return !!(
+                this.currentTime > 0 &&
+                !this.paused &&
+                !this.ended &&
+                this.readyState > 2
+            );
+        },
+    });
 })();
 
-$(document).ready(function () {
-  $(".burger, .shtor,  .close_menu").click(function (event) {
-    $(".burger").toggleClass("burger_active");
-    $(".nav").toggleClass("active_nav");
-    $(".shtor").toggleClass("active_shtor");
-    $("body").toggleClass("body_scrl");
-    $(".close_menu").toggleClass("close_menu_active ");
-    $(".language").toggleClass("language_active");
-  });
 
-  const video = $("#video")[0];
-  $(".video_modal__close").on("click", function () {
-    $(".video_modal").removeClass("video_modal__active");
-    $("body").removeClass("body_scrl");
-    video.pause();
-  });
 
-  $(".play").on("click", (e) => {
-    if (e.currentTarget.dataset.movie__link) {
-      video.src = e.currentTarget.dataset.movie__link;
-    } else {
-      video.src = "./images/movie.mp4";
-    }
+$(document).ready(function() {
+    // butger
+    $(".burger, .shtor,  .close_menu").click(function(event) {
+        $(".burger").toggleClass("burger_active");
+        $(".nav").toggleClass("active_nav");
+        $(".shtor").toggleClass("active_shtor");
+        $("body").toggleClass("body_scrl");
+        $(".close_menu").toggleClass("close_menu_active ");
+        $(".language").toggleClass("language_active");
+    });
 
-    $(".video_modal").addClass("video_modal__active");
-    $("body").addClass("body_scrl");
-    $(".video__play").addClass("video__play_hidden");
-    video.play();
-  });
 
-  $(".copy_link").on("click", (e) => {
-    if (e.target.dataset.link) {
-      navigator.clipboard.writeText(e.target.dataset.link).then(() => {
-        e.target.className = "primary";
-        e.target.innerText = "Скопировано";
-      });
-    } else {
-      e.target.className = "danger";
-      e.target.innerText = "Ссылка пуста";
-    }
-    setTimeout(() => {
-      e.target.className = "copy_link";
-      e.target.innerHTML = 'Копировать <img src="/images/copy.svg" alt="">';
-    }, 2000);
-  });
+    //video modal
+    const video = $("#video")[0];
 
-  var button = $("#button-up");
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      button.fadeIn();
-    } else {
-      button.fadeOut();
-    }
-  });
-  button.on("click", function () {
-    $("body, html").animate(
-      {
-        scrollTop: 0,
-      },
-      800
-    );
-    return false;
-  });
 
-  $(".images img").click(function () {
-    $("#full-image").attr("src", $(this).attr("src"));
-    $("#image-viewer").show();
-  });
+    $(".video_modal__close").on("click", function() {
+        $(".video_modal").removeClass("video_modal__active");
+        $("body").removeClass("body_scrl");
+        video.pause();
+    });
 
-  $("#image-viewer .close").click(function () {
-    $("#image-viewer").hide();
-  });
+    $(".play").on("click", (e) => {
+        if (e.currentTarget.dataset.movie__link) {
+            video.src = e.currentTarget.dataset.movie__link;
+        } else {
+            video.src = "./images/movie.mp4";
+        }
+
+        $(".video_modal").addClass("video_modal__active");
+        $("body").addClass("body_scrl");
+        $(".video__play").addClass("video__play_hidden");
+        video.play();
+    });
+
+
+    //copy link
+    $(".copy_link").on("click", (e) => {
+        if (e.target.dataset.link) {
+            navigator.clipboard.writeText(e.target.dataset.link).then(() => {
+                e.target.className = "primary";
+                e.target.innerText = "Скопировано";
+            });
+        } else {
+            e.target.className = "danger";
+            e.target.innerText = "Ссылка пуста";
+        }
+        setTimeout(() => {
+            e.target.className = "copy_link";
+            e.target.innerHTML = 'Копировать <img src="/images/copy.svg" alt="">';
+        }, 2000);
+    });
+
+
+    //button up
+    var button = $("#button-up");
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+    button.on("click", function() {
+        $("body, html").animate({
+                scrollTop: 0,
+            },
+            800
+        );
+        return false;
+    });
+
+
+    //poster img modal
+    $(".images img").click(function() {
+        $("#full-image").attr("src", $(this).attr("src"));
+        $("#image-viewer").show();
+    });
+
+    $("#image-viewer .close").click(function() {
+        $("#image-viewer").hide();
+    });
 });
